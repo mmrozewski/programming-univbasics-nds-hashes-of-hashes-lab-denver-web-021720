aba7621d8a9b2b5c16de230817f70b81c9e5c5bf
Mateusz Mrozewski <mmrozews@gmail.com>
2020-02-08 00:22:52 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -31,7 +31,26 @@
 
 def naming_system
   
-  {label: 
+  { label: "Kingdom",
+    sub_category: {
+      label: "Phylum",
+      sub_category: {
+        label: "Class",
+        sub_category: {
+          label: "Order",
+          sub_category: {
+            label: "Family",
+            sub_category: {
+              label: "Genus",
+              sub_category: {
+                label: "Species"
+              }
+            }
+          }
+        }
+     }
+  }
+ }
   
   
   #  Kingdom
